6b213e381e3615d75651d6ffef576edcefb92b9f
nl900 <nancy.lee900@gmail.com>
2019-09-26 20:37:27 +0530

    password reset

--- petstore/settings.py
+++ petstore/settings.py
@@ -122,3 +122,6 @@ STATIC_URL = '/static/'
 
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
 LOGIN_REDIRECT_URL = '/'
+
+#Log emails sent to console
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
