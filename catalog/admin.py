d41a97c23a19e382c6e223b861e74e52b49c7d4f
nl900 <nancy.lee900@gmail.com>
2019-09-26 14:31:48 +0530

    create view for homepage

--- catalog/admin.py
+++ catalog/admin.py
@@ -3,18 +3,21 @@ from django.contrib import admin
 from catalog.models import Animal, Pet, Breeder
 # Register your models here.
 
-admin.site.register(Animal)
-admin.site.register(Pet)
-admin.site.register(Breeder)
-
-
+@admin.register(Pet)
 class PetAdmin(admin.ModelAdmin):
     list_select_related = ('animal', 'breeder',)
+    list_display = ('id', 'animal')
+
     class Meta:
         model = Pet
 
+@admin.register(Animal)
 class AnimalAdmin(admin.ModelAdmin):
     list_select_related = ('parent',)
     list_display = ('name', 'parent', )
     class Meta:
         model = Animal
+
+@admin.register(Breeder)
+class BreederAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address')
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from catalog.models import Animal, Pet, Breeder
 
 # Create your views here.
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_animal = Animal.objects.all().count()
+    
+    # Available (status = 'a')
+    pet_available = Pet.objects.filter(status__exact='a').count()
+    
+    # The 'all()' is implied by default.    
+    num_breeder = Breeder.objects.count()
+    
+    context = {
+        'num_animal': num_animal,
+        'num_pet_available': pet_available ,
+        'num_breeder': num_breeder,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
